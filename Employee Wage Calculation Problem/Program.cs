ab85df4e52ab18e37b981c6aebaaef928e4ed5db
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-25 14:48:40 +0530

    [Sai]Refactor. Present or abscent

--- Employee Wage Calculation Problem/Program.cs
+++ Employee Wage Calculation Problem/Program.cs
@@ -7,6 +7,22 @@ namespace Employee_Wage_Calculation_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage problem!");
+
+            Random rand = new Random();
+
+            int is_attended = rand.Next(0, 2);
+
+            int attended = 1;
+
+
+            if(is_attended == attended)
+            {
+                Console.Out.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.Out.WriteLine("Employee is absent");
+            }
         }
     }
 }
