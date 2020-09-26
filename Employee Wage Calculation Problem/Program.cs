2d33f4c52a306ac2e8b795475b06485bebd1a994
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-26 13:29:04 +0530

    [Sai] Reverted. clear

--- Employee Wage Calculation Problem/Program.cs
+++ Employee Wage Calculation Problem/Program.cs
@@ -13,23 +13,23 @@ namespace Employee_Wage_Calculation_Problem
             int wage = 20;
 
             //variable
-            int type = rand.Next(0,3);
+            int type = rand.Next(0, 3);
             int hours = 0;
             int total_wage = 0;
-            
-            if (is_full_time == type)
-            {
-                hours = 8;
 
-            }
-            else if(is_part_time == type)
+            switch (type)
             {
-                hours = 4;
+                case 1:
+                    hours = 8;
+                    break;
+                case 2:
+                    hours = 4;
+                    break;
+                default:
+                    hours = 0;
+                    break;
             }
-            else
-            {
-                hours = 0;
-            }
+
 
             total_wage = hours * wage;
 
