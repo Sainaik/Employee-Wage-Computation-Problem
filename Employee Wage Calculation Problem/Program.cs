38747e15ea679a1a34a0a078005ee4262c1ede46
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-27 17:24:13 +0530

    [Sai] Final COmmit

--- Employee Wage Calculation Problem/Program.cs
+++ Employee Wage Calculation Problem/Program.cs
@@ -7,49 +7,8 @@ namespace Employee_Wage_Calculation_Problem
         static void Main(string[] args)
         {
 
-            Random rand = new Random();
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int TOTAL_WORKING_HOURS = 100;
-            const int TOTAL_WORKING_DAYS = 20;
-            const int WAGE_PER_HOUR = 20;
-
-            //variable
-
-            int numberOfHours = 0;
-            int workingDays = 0;
-            int total_wage = 0;
-
-
-            while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
-            {
-                int type = rand.Next(0, 3);
-                int hours = 0;
-                switch (type)
-                {
-                    case IS_FULL_TIME:
-                        hours = 8;
-                        break;
-                    case IS_PART_TIME:
-                        hours = 4;
-                        break;
-                    default:
-                        hours = 0;
-                        break;
-
-                }
-
-                numberOfHours += hours;
-
-                workingDays++;
-
-
-                Console.Out.WriteLine("Day "+workingDays+" hours "+ hours);
-            }
-
-            total_wage = numberOfHours * WAGE_PER_HOUR;
-
-            Console.Out.WriteLine("Total wage of the month :" + total_wage);
+            
+            Console.Out.WriteLine("Welcome to Employee Wage Calculation Problem");
 
         }
 
